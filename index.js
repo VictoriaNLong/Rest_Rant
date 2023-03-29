89816d98056830dc36bd4010588b47f16e3caa20
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-03-29 15:13:47 -0700

    added stylesheet

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => { 
