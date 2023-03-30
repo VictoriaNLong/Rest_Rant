9911172e4d8dc4be6276aa2b7ec43c3271d82873
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-03-29 17:34:32 -0700

    Can add new places

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,6 @@
 const router = require('express').Router()
+let places = require('../models/places')
 
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/thaipic.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/catcafepic.jpg'
-  }]
 
   router.get('/new', (req, res) => {
     res.render('places/new')
@@ -23,8 +11,20 @@ router.get('/', (req, res) => {
 }) 
 
 router.post('/', (req, res) => {
-    console.log(req.body)
-    res.send('POST /places')
-})
+    if (!req.body.pic) {
+      req.body.pic = 'http://placekitten.com/400/400'
+    }
+    if (!req.body.city) {
+      req.body.city = 'Anytown'
+    }
+    if (!req.body.state) {
+      req.body.state = 'USA'
+    }
+
+    places.push(req.body)
+
+    res.redirect('/places')
+  })
+  
 
 module.exports = router
