b12f2826af185da68cbefb8d00b16724fe019af8
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-04-01 13:30:15 -0700

    added editing, cant resolve error

--- controllers/places.js
+++ controllers/places.js
@@ -8,13 +8,10 @@ let places = require('../models/places')
   
   router.get('/:id/edit', (req, res) => {
     let id = Number(req.params.id)
-    if (isNaN(id)) {
+    if (isNaN(id) || !places[id]) {
         res.render('error404')
-    } else if (!places[id]) {
-      res.render('error404')
-    } else {
-    res.render('places/edit', { place: places[id], id })
     }
+    res.render('places/edit', { place: places[id], id })
 })
   
 
@@ -25,7 +22,7 @@ let places = require('../models/places')
     } else if (!places[id]) {
         res.render('error404')
     } else {
-    res.render('places/show', {place: places[id], id})
+    res.render('places/show', { place: places[id], id })
     }
   })
 
@@ -41,26 +38,31 @@ let places = require('../models/places')
     }
   })
   
+  router.put('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id) || !places[id]) {
+        res.render('error404')
+    }
+
+    req.body.pic = req.body.pic || '/images/default_food.jpg'
+    req.body.city = req.body.city || 'Anytown'
+    req.body.state = req.body.state || 'USA'
+    places[id] = req.body
+    res.redirect(`/places/${id}`)
+})
   
 router.get('/', (req, res) => {
     res.render('places/index', { places })
 }) 
 
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
+  req.body.pic = req.body.pic || '/images/default_food.jpg'
+  req.body.city = req.body.city || 'Anytown'
+  req.body.state = req.body.state || 'USA'
 
-    places.push(req.body)
-
-    res.redirect('/places')
-  })
+  places.push(req.body)
+  res.redirect('/places')
+})
   
 
 module.exports = router
