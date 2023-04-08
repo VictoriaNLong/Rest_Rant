cb888f7cb3c53dac2641388b778d3be73cdcd2b9
VictoriaNLong <108419011+VictoriaNLong@users.noreply.github.com>
2023-04-08 16:59:59 -0700

    added validation error

--- models/places.js
+++ models/places.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose')
 
 const placeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  pic: String,
+  pic:  { type: String, default: 'http://placekitten.com/350/350' },
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
-  founded: Number,
+  founded: {type: Number, min: [1673, 'Too old'], max: [new Date().getFullYear(), 'Date in the future']}
 })
+
+placeSchema.methods.showEstablished = function() {
+  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+}
+
 
 module.exports = mongoose.model('Place', placeSchema)
